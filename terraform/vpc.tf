fbea123f3e79ea24762d7d441871782b64b427c6
heeega <m_mny1004@naver.com>
2026-08-03 16:18:40 +0900

    feat: create VPC with 3-tier subnets (web/app/db) via Terraform

---
+++ terraform/vpc.tf
@@ -0,0 +1,40 @@
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "soc-3tier-vpc"
+  }
+}
+
+resource "aws_subnet" "web" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block               = "10.0.1.0/24"
+  availability_zone        = "ap-northeast-2a"
+  map_public_ip_on_launch  = true
+
+  tags = {
+    Name = "web-tier-subnet"
+  }
+}
+
+resource "aws_subnet" "app" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "ap-northeast-2a"
+
+  tags = {
+    Name = "app-tier-subnet"
+  }
+}
+
+resource "aws_subnet" "db" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.3.0/24"
+  availability_zone = "ap-northeast-2a"
+
+  tags = {
+    Name = "db-tier-subnet"
+  }
+}
