2d59aa7868af1558e195aef70b0046b7855a7c0d
heeega <m_mny1004@naver.com>
2026-08-15 14:37:45 +0900

    ci: add terraform plan workflow with OIDC auth

---
+++ terraform/github_oidc.tf
@@ -0,0 +1,42 @@
+# ---------- GitHub Actions OIDC Provider ----------
+resource "aws_iam_openid_connect_provider" "github" {
+  url             = "https://token.actions.githubusercontent.com"
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
+}
+
+# ---------- GitHub Actions가 위임받을 IAM Role ----------
+resource "aws_iam_role" "github_actions" {
+  name = "${var.project_name}-github-actions-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Federated = aws_iam_openid_connect_provider.github.arn
+        }
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Condition = {
+          StringEquals = {
+            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+          }
+          StringLike = {
+            "token.actions.githubusercontent.com:sub" = "repo:heeega/aws-3tier-soc-monitoring:*"
+          }
+        }
+      }
+    ]
+  })
+
+  tags = {
+    Name = "${var.project_name}-github-actions-role"
+  }
+}
+
+# ---------- Terraform plan에 필요한 권한 (토이 프로젝트 범위상 AdministratorAccess, 추후 축소 예정) ----------
+resource "aws_iam_role_policy_attachment" "github_actions_admin" {
+  role       = aws_iam_role.github_actions.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
